302743af9cb43db68effcf8501c3dfea1cad60e9
Hendrik Schäfer <hendrikschafer@hendriks-mbp-2.speedport.ip>
2019-10-20 12:27:23 +0200

    new form

--- src/pages/dev.js
+++ src/pages/dev.js
@@ -10,26 +10,26 @@ const SecondPage = () => (
   <Helmet>
     <meta name="robots" content="noindex, nofollow" />
   </Helmet>
-  
+
   <div className="bg-washed-red pv5 flex flex-column items-center">
     <h1 className="db f1 display fw1">Development</h1>
   </div>
   <div className="lh-copy f4 serif mw8 center">
-  <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+  <form name="contact" method="post">
+         <input type="hidden" name="form-name" value="contact" />
+         <p>
+           <label>Ihr Name: <input type="text" name="name"/></label>
+         </p>
+         <p>
+           <label>Ihre Email: <input type="email" name="email"/></label>
+         </p>
+         <p>
+           <label>Ihre Nachricht: <textarea name="message"></textarea></label>
+         </p>
+         <p>
+           <button type="submit">Absenden</button>
+         </p>
+       </form>
   </div>
   </Layout>
 )
